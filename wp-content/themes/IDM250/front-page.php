9422cd4539165569e8c230fcb87efab36c9636c9
111yp <42048931+111yp@users.noreply.github.com>
2020-08-24 16:17:16 -0400

    update front page
    update front page

--- wp-content/themes/IDM250/front-page.php
+++ wp-content/themes/IDM250/front-page.php
@@ -8,5 +8,56 @@
  */
 
 get_header();?>
-Front Page
+<body>
+    <div class="fp__content">
+        <div class="fp__gallery">
+            <div class="fp__column fp__first">
+                <img src="/wp-content/themes/IDM250/dist/images/image-1.png">
+                <img src="/wp-content/themes/IDM250/dist/images/image-2.png">
+            </div>
+            <div class="fp__column fp__second">
+                <img src="/wp-content/themes/IDM250/dist/images/image-3.png">
+                <img src="/wp-content/themes/IDM250/dist/images/image-4.png">
+            </div>
+        </div>
+        <div class="fp__text">
+            <h1>UI • UX</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sit amet magna nec nisi malesuada pharetra. Cras sit amet orci ipsum. Nam gravida ullamcorper arcu, eu auctor elit sollicitudin sit amet. Suspendisse efficitur lacus vel porttitor elementum. Proin justo metus, ultrices eu nulla et, suscipit molestie orci. Nulla tincidunt tincidunt risus, eget euismod leo lobortis sit amet. Praesent imperdiet, ligula a tincidunt porttitor, magna magna scelerisque elit, sit amet facilisis diam purus eu ipsum. Cras nec fringilla magna, ac tristique lacus. </p>
+        </div>
+    </div>
+
+    <div class="fp__content">
+        <div class="fp__text">
+            <h1>Art</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sit amet magna nec nisi malesuada pharetra. Cras sit amet orci ipsum. Nam gravida ullamcorper arcu, eu auctor elit sollicitudin sit amet. Suspendisse efficitur lacus vel porttitor elementum. Proin justo metus, ultrices eu nulla et, suscipit molestie orci. Nulla tincidunt tincidunt risus, eget euismod leo lobortis sit amet. Praesent imperdiet, ligula a tincidunt porttitor, magna magna scelerisque elit, sit amet facilisis diam purus eu ipsum. Cras nec fringilla magna, ac tristique lacus. </p>
+        </div>
+        <div class="fp__gallery">
+            <div class="fp__column fp__first">
+                <img src="/wp-content/themes/IDM250/dist/images/image-1.png">
+                <img src="/wp-content/themes/IDM250/dist/images/image-2.png">
+            </div>
+            <div class="fp__column fp__second">
+                <img src="/wp-content/themes/IDM250/dist/images/image-3.png">
+                <img src="/wp-content/themes/IDM250/dist/images/image-4.png">
+            </div>
+        </div>
+    </div>
+
+    <div class="fp__content fp__footer__bg">
+        <div class="fp__gallery fp__gallery__icon">
+            <div class="fp__column fp__column__icon">
+                <img src="/wp-content/themes/IDM250/dist/images/image-5.jpg">
+            </div>
+        </div>
+        <div class="fp__text fp__text__footer">
+            <h1>UI • UX</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sit amet magna nec nisi malesuada pharetra. Cras sit amet orci ipsum. Nam gravida ullamcorper arcu, eu auctor elit sollicitudin sit amet. Suspendisse efficitur lacus vel porttitor elementum. Proin justo metus, ultrices eu nulla et, suscipit molestie orci. </p>
+            <p>Phone: XXX XXXX XXXX</p>
+            <p>Email:  XXXXXXXXXXX@gmail.com</p>
+        </div>
+    </div>
+
+    <div class="bg"></div>
+    
+</body>
 <?php get_footer(); ?>
