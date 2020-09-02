3a54799dede8683d3befb6efdd8266ac50087791
111yp <42048931+111yp@users.noreply.github.com>
2020-09-02 19:05:33 -0400

    add 404.php
    add 404.php

---
+++ wp-content/themes/IDM250/404.php
@@ -0,0 +1,55 @@
+<?php get_header();?>
+<main class="main-content">
+  <div class="content">
+  <body>
+    <div class="fp__content hide">
+        <div class="fp__gallery">
+            <div class="fp__column fp__first">
+                <img src="wp-content/themes/IDM250/dist/images/image-1.png">
+                <img src="wp-content/themes/IDM250/dist/images/image-2.png">
+            </div>
+            <div class="fp__column fp__second">
+                <img src="wp-content/themes/IDM250/dist/images/image-3.png">
+                <img src="wp-content/themes/IDM250/dist/images/image-4.png">
+            </div>
+        </div>
+        <div class="fp__text">
+            <h1>UI • UX</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sit amet magna nec nisi malesuada pharetra. Cras sit amet orci ipsum. Nam gravida ullamcorper arcu, eu auctor elit sollicitudin sit amet. Suspendisse efficitur lacus vel porttitor elementum. Proin justo metus, ultrices eu nulla et, suscipit molestie orci. Nulla tincidunt tincidunt risus, eget euismod leo lobortis sit amet. Praesent imperdiet, ligula a tincidunt porttitor, magna magna scelerisque elit, sit amet facilisis diam purus eu ipsum. Cras nec fringilla magna, ac tristique lacus. </p>
+        </div>
+    </div>
+
+    <div class="fp__content hide">
+        <div class="fp__text">
+            <h1>Art</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sit amet magna nec nisi malesuada pharetra. Cras sit amet orci ipsum. Nam gravida ullamcorper arcu, eu auctor elit sollicitudin sit amet. Suspendisse efficitur lacus vel porttitor elementum. Proin justo metus, ultrices eu nulla et, suscipit molestie orci. Nulla tincidunt tincidunt risus, eget euismod leo lobortis sit amet. Praesent imperdiet, ligula a tincidunt porttitor, magna magna scelerisque elit, sit amet facilisis diam purus eu ipsum. Cras nec fringilla magna, ac tristique lacus. </p>
+        </div>
+        <div class="fp__gallery">
+            <div class="fp__column fp__first">
+                <img src="wp-content/themes/IDM250/dist/images/image-1.png">
+                <img src="wp-content/themes/IDM250/dist/images/image-2.png">
+            </div>
+            <div class="fp__column fp__second">
+                <img src="wp-content/themes/IDM250/dist/images/image-3.png">
+                <img src="wp-content/themes/IDM250/dist/images/image-4.png">
+            </div>
+        </div>
+    </div>
+
+    <div class="fp__content fp__footer__bg">
+      <div class="fp__text fp__text__footer">
+        <div class="fourofour-container">
+          <h1>Uh oh...</h1>
+          <h2>PAGE NOT FOUND</h2>
+          <p>The rabbits must have been nibbling the cables again...</p>
+          <p>Maybe this will help you <a href="https://example.com">Home</a></p>
+        </div>
+      </div>
+    </div>
+
+    <div class="bg"></div>
+    
+</body>
+  </div>
+</main>
+<?php get_footer(); ?>
